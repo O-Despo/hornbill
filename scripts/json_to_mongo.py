6bfe97add805c4065f496c2b1d4c1acc9a163519
= <=>
2021-12-18 15:39:16 -0500

    Finished json_to_mongo.py / mongo env cols from - > _

--- scripts/json_to_mongo.py
+++ scripts/json_to_mongo.py
@@ -1,21 +1,21 @@
-#!/bin/python
 """
 A script that takes a JSON file and imports it into mongo Col.
 This is be used to restore Mongo backups
 """
 import sys
-import os
 import json
-from dotenv import dotenv_values
+from dotenv import dotenv_values 
 from pymongo import MongoClient
 
-config = dotenv_values('../.env')
-
-client = MongoClient(config.MONGO)
-db = client[config.MONGO_DB]
-col = db[config.MONGO_COL_TRENDS]
+config = dotenv_values()
+client = MongoClient(config['MONGO'])
+db = client[config['MONGO_DB']]
+col = db[config['MONGO_COL_TRENDS']]
 
 json_loc = sys.argv[1]
 json_to_load = json.load(open(json_loc))
 
-col.insert_many(json_to_load)
+#_id is removed to prevent id error when entering into mongo
+for item in json_to_load:
+    del item['_id']
+    col.insert_one(item)
